f77b0b98cd7089fd1d73d43f36dbcaae777126db
bbbradsmith <bbbradsmith@users.noreply.github.com>
2019-02-14 10:57:43 -0500

    finish 5.0.6

--- src/version.h
+++ src/version.h
@@ -1,3 +1,3 @@
-#define VERSION "5.0.5"
-#define LONGVERSION "CopyNES Blue v5.0.5"
-#define APPTITLE "USB CopyNES Blue Win32 Client v5.0.5"
+#define VERSION "5.0.6"
+#define LONGVERSION "CopyNES Blue v5.0.6"
+#define APPTITLE "USB CopyNES Blue Win32 Client v5.0.6"
